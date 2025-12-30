b21b369927cca33f171c445106c0a48a63aab8ab
surya021104 <suryaramya111111@gmail.com>
2025-12-30 17:38:29 +0530

    Update IssuePage.jsx

--- src/modules/issues/IssuePage.jsx
+++ src/modules/issues/IssuePage.jsx
@@ -2,13 +2,18 @@ import IssueHeader from "./IssueHeader";
 import IssueTable from "./IssueTable";
 import useIssues from "../../hooks/useIssues";
 
-export default function IssuePage() {
+export default function IssuePage({ currentUser }) {
   const { issues, addIssue, updateStatus } = useIssues();
 
   return (
     <>
-      <IssueHeader onAddIssue={addIssue} />
-      <IssueTable issues={issues} onStatusChange={updateStatus} />
+      <IssueHeader onAddIssue={(title) => addIssue(title, currentUser)} />
+      <IssueTable
+        issues={issues}
+        onStatusChange={(id, status) =>
+          updateStatus(id, status, currentUser)
+        }
+      />
     </>
   );
 }
