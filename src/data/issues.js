cc49ba219a5e25d77ae72d2597c316c4d9fc2829
surya021104 <suryaramya111111@gmail.com>
2025-12-30 15:43:56 +0530

    Create issues.js

---
+++ src/data/issues.js
@@ -0,0 +1,23 @@
+
+export const issues = [
+  {
+    id: "BUG-001",
+    title: "Login crash",
+    issueType: "LOGIN_CRASH",
+    module: "Frontend",
+    status: "Reopened",
+    assignee: "Frontend Team",
+    createdAt: "30/12/2025, 15:21:11",
+    resolvedAt: "30/12/2025, 15:21:11"
+  },
+  {
+    id: "BUG-002",
+    title: "Server crashed",
+    issueType: "SERVER_CRASH",
+    module: "Frontend",
+    status: "Open",
+    assignee: "Frontend Team",
+    createdAt: "30/12/2025, 15:30:00",
+    resolvedAt: null
+  }
+];
